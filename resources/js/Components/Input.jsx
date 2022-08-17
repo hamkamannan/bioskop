ac1812d8e8e88e8e3bf114f5907e2f691c763a81
Hamka Mannan <hamka@mannan.id>
2022-08-17 12:31:34 +0700

    feat: slicing Dashboard

--- resources/js/Components/Input.jsx
+++ resources/js/Components/Input.jsx
@@ -1,14 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
+import PropType from "prop-types";
+
+Input.propTypes = {
+    type: PropType.oneOf(["text", "email", "password", "number", "file"]),
+    name: PropType.string,
+    value: PropType.oneOfType([PropType.string, PropType.number]),
+    defaultValue: PropType.oneOfType([PropType.string, PropType.number]),
+    className: PropType.string,
+    variant: PropType.oneOf(["primary", "error", "primary-outline"]),
+    autoComplete: PropType.string,
+    required: PropType.bool,
+    isFocused: PropType.bool,
+    handleChange: PropType.func,
+    placeholder: PropType.string,
+    isError: PropType.bool,
+};
 
 export default function Input({
-    type = 'text',
+    type = "text",
     name,
     value,
+    defaultValue,
     className,
+    variant = "primary",
     autoComplete,
     required,
     isFocused,
     handleChange,
+    placeholder,
+    isError,
 }) {
     const input = useRef();
 
@@ -24,14 +44,15 @@ export default function Input({
                 type={type}
                 name={name}
                 value={value}
-                className={
-                    `border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm ` +
-                    className
-                }
+                defaultValue={defaultValue}
+                className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full ${
+                    isError && "input-error"
+                } input-${variant} ${className}`}
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
                 onChange={(e) => handleChange(e)}
+                placeholder={placeholder}
             />
         </div>
     );
--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,45 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('admin', function(){
-	return 'Hi Admin';
-})->middleware('role:admin');
 
-Route::get('user', function(){
-	return 'Hi User';
-})->middleware('role:user');
+// Route::post('midtrans/notification', [SubscriptionPlanController::class, 'midtransCallback']);
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::redirect('/', '/prototype/login');
+
+// Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function () {
+//     Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+//     Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show')->middleware('checkUserSubscription:true');
+
+//     Route::get('subscription-plan', [SubscriptionPlanController::class, 'index'])->name('subscriptionPlan.index')->middleware('checkUserSubscription:false');
+//     Route::post('subscription-plan/{subscriptionPlan}/user-subscribe', [SubscriptionPlanController::class, 'userSubscribe'])->name('subscriptionPlan.userSubscribe')->middleware('checkUserSubscription:false');
+// });
+
+// Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.dashboard.')->group(function () {
+//     Route::put('movie/{movie}/restore', [AdminMovieController::class, 'restore'])->name('movie.restore');
+//     Route::resource('movie', AdminMovieController::class);
+// });
+
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+
+    route::get('/register', function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
+
+    route::get('/dashboard', function () {
+        return Inertia::render('Prototype/Dashboard');
+    })->name('dashboard');
+
+    route::get('/subscriptionPlan', function () {
+        return Inertia::render('Prototype/SubscriptionPlan');
+    })->name('subscriptionPlan');
+
+    route::get('/movie/{slug}', function () {
+        return Inertia::render('Prototype/Movie/Show');
+    })->name('movie.show');
 });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
